039817e310cc4ef8d6bf08e6f48aba03fafee73a
unknown <593496637@qq.com>
2020-03-18 11:10:58 +0800

    更新main

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App'
+import base from './base'
+import store from './store'
 
 Vue.config.productionTip = false
+Vue.use(base); //将全局函数当做插件来进行注册
 
 App.mpType = 'app'
+
 
 const app = new Vue({
 	...App
